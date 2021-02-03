5c7a64f2ba40b5d1f3ee37de0c8b042b84ad7132
lars <l.rook2002@hotmail.nl>
2021-02-03 10:06:35 +0100

    [ADD] Added a new project: 1.B.2

---
+++ Microcontrollers/Opdracht 1.B.1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht 1.B.1.c
+ *
+ * Created: 3-2-2021 10:03:57
+ * Author : Gebruiker
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Microcontrollers/Opdracht 1.B.2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht 1.B.2.c
+ *
+ * Created: 3-2-2021 10:05:27
+ * Author : Gebruiker
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
