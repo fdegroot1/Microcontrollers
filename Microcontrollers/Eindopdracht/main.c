cf9fc121eac9cd06974fde610d2e96576baa498a
Fabjuuuh <f.degroot1@student.avans.nl>
2021-03-24 10:15:58 +0100

    opdrachten af

---
+++ Microcontrollers/Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 24/03/2021 10:15:18
+ * Author : fabia
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
--- Microcontrollers/Opdracht 3.B.3/main.c
+++ Microcontrollers/Opdracht 3.B.3/main.c
@@ -5,14 +5,70 @@
  * Author : fabia
  */ 
 
+#define F_CPU 8e6
+
 #include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+volatile int counter = 0;
+int light_state = 0; 
 
+ISR(TIMER2_OVF_vect){
+	
+	TCNT2 = -8;
+	if(counter == 15 && light_state == 0){
+		light_state = 1;
+		counter = 0;
+		PORTD = 0;
+	}
+	else if(counter == 25 && light_state == 1){
+		light_state	= 0;
+		counter = 0;
+		PORTD = 1<<7;
+	}
+	counter++;
+	
+}
+
+ISR(TIMER2_COMP_vect){
+	TCNT2 = -8;
+	if(counter == 15 && light_state == 0){
+		light_state = 1;
+		counter = 0;
+		PORTD = 0;
+	}
+	else if(counter == 25 && light_state == 1){
+		light_state	= 0;
+		counter = 0;
+		PORTD = 1<<7;
+	}
+	counter++;
+	
+}
+
+void initTimer2(void){
+	TCNT2 = -8;
+	TIMSK |= 1<<6; //ISR overflow
+	sei();
+	TCCR2 = 0x05;
+	
+}
 int main(void)
 {
     /* Replace with your application code */
+	DDRD = 1<<7; // set PORTD.7 for output
+	PORTD = 1<<7;
+	
+	initTimer2();
+	
+	
+	
+		
     while (1) 
     {
+		_delay_ms(1);
     }
 }
 
